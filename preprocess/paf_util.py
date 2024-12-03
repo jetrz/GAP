59f2f4c2da2ce5b283474cc2b31fe321d70c06a5
jetrz <joshuaemmanuelteo@gmail.com>
2024-12-03 10:22:57 +0800

    remove mmap

--- preprocess/paf_util.py
+++ preprocess/paf_util.py
@@ -432,13 +432,14 @@ def parse_paf(paths, aux):
     print("Generating r2s...")
     r2s = parse_fasta(paths['ec_reads'])
     if paths['ul_reads']: r2s.update(parse_fasta(paths['ul_reads']))
-    temp_r2s_file = f'/mnt/sod2-project/csb4/wgs/lovro_interns/joshua/temp_r2s_{random.randint(1,9999999)}.pkl'
-    with open(temp_r2s_file, "wb") as f:
-        pickle.dump(r2s, f)
-    del r2s; gc.collect()
-    with open(temp_r2s_file, "rb") as f:
-        mmapped = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-        R2S = pickle.loads(mmapped)
+    # temp_r2s_file = f'/mnt/sod2-project/csb4/wgs/lovro_interns/joshua/temp_r2s_{random.randint(1,9999999)}.pkl'
+    # with open(temp_r2s_file, "wb") as f:
+    #     pickle.dump(r2s, f)
+    # del r2s; gc.collect()
+    # with open(temp_r2s_file, "rb") as f:
+    #     mmapped = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    #     R2S = pickle.loads(mmapped)
+    R2S = r2s
 
     for c_n_id in sorted(N2R.keys()):
         if c_n_id % 2 != 0: continue # Skip all virtual nodes
@@ -508,8 +509,8 @@ def parse_paf(paths, aux):
         'ghost_nodes' : ghosts
     }
 
-    mmapped.close()
-    os.remove(temp_r2s_file)
+    # mmapped.close()
+    # os.remove(temp_r2s_file)
 
     return data
 
