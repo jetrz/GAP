e6e3c9d6d1c966685475df6878f9e4cdf1d1a341
jetrz <joshuaemmanuelteo@gmail.com>
2024-12-10 17:03:39 +0800

    minor bugfix

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -920,9 +920,7 @@ def run_postprocessing(config):
         aux['hifi_r2s'] = Fasta(paths['ec_reads'])
         aux['ul_r2s'] = Fasta(paths['ul_reads']) if paths['ul_reads'] else None
 
-        # postprocess(genome, hyperparams=postprocessing_config, paths=paths)
-        for use_telomere_info in [True, False]:
-            postprocessing_config['use_telomere_info'] = use_telomere_info
-            for w in [0.005, 0.0025, 0.001]:
-                postprocessing_config['walk_valid_p'] = w
-                postprocess(genome, hyperparams=postprocessing_config, paths=paths, aux=aux)
+        # postprocess(genome, hyperparams=postprocessing_config, paths=paths, aux=aux)
+        for w in [0.05, 0.025, 0.01]:
+            postprocessing_config['walk_valid_p'] = w
+            postprocess(genome, hyperparams=postprocessing_config, paths=paths, aux=aux)
